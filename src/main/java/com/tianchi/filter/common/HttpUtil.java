0541fa2c807035e7495b5fad03312cdb0e78a698
15893883880@163.com <HENg626683>
2020-05-11 09:31:27 +0800

    增加工具类

---
+++ src/main/java/com/tianchi/filter/common/HttpUtil.java
@@ -0,0 +1,11 @@
+package com.tianchi.filter.common;
+
+/**
+ * @Author: zhouheng
+ * @Created: with IntelliJ IDEA.
+ * @Description:
+ * @Date: 2020-05-11
+ * @Time: 09:31
+ */
+public class HttpUtil {
+}
